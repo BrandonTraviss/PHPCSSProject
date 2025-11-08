747eefd9028bf03baa96c5c426a77b72f05e3cc0
BrandonTraviss <btraviss1987@gmail.com>
2025-11-08 00:19:44 -0500

    Added Meta/Header/Footer to all pages

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
+    $pageTitle = "AracdiaWorks";
+    $pageDescription = "Explore ArcadiaWorks handcrafted vintage arcade cabinets—authentic retro gaming experiences built with modern precision. Custom designs, premium materials, and timeless nostalgia for collectors and enthusiasts.";
     include_once "./inc/templates/meta.php";
     include_once "./inc/templates/header.php";
 ?>
 <body>
-    
+    <main>
+        
+    </main>
 </body>
 <?php include_once "./inc/templates/footer.php" ?>
