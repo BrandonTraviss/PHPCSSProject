a864b00fd6f7eb59ca3d9adce16e80426cb447ff
BrandonTraviss <btraviss1987@gmail.com>
2025-11-13 11:05:54 -0500

    FileHandler - Commented

--- inc/classes/FileHandler.php
+++ inc/classes/FileHandler.php
@@ -1,19 +1,23 @@
 <?php
 class FileHandler
 {
+    // Save an image
     public function saveImage(array $fileData, string $fileExt)
     {
+        // Creates filename
         $newFileName = uniqid('', true) . '.' . $fileExt;
         $destination = 'upload/' . $newFileName;
-
+        // returns the link to the file to be used as imgLink
         if (move_uploaded_file($fileData['tmp_name'], $destination)) {
             return $destination;
         }
 
         return false;
     }
+    // Delete an image
         public function deleteImage(string $imgLink)
     {
+            // Checks if the file exists and deletes it
             if (file_exists($imgLink)) {
                 return unlink($imgLink);
             }
